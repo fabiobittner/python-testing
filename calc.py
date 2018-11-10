fb80140e41977eb496261db2a2d8ac7b8c5c356d
Fabio Bittner <36137036+fabiobittner@users.noreply.github.com>
2018-11-10 10:55:03 +0100

    Add calculator module with doctest

---
+++ calc.py
@@ -0,0 +1,14 @@
+"""
+A calculator - with doctests
+"""
+
+
+def add(a, b):
+    """Adds a and b together
+    Examples:
+    >>> add(1, 2)
+    3
+    >>> add(-1, 3)
+    2
+    """
+    return a + b
